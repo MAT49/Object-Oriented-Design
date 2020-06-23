8c6c36fa4b54470445e3faecb5fab348d993685d
Ha Hwang <spyderthing@gmail.com>
2020-06-22 23:14:34 -0400

    Added DeckOfCards class

--- ObjectOrientedDesign/src/main/java/me/nonnon/DeckOfCards.java
+++ ObjectOrientedDesign/src/main/java/me/nonnon/DeckOfCards.java
@@ -1,5 +1,67 @@
 package me.nonnon;
 
+import java.util.ArrayList;
+
 public class DeckOfCards {
+	// Design the data structures for a generic deck of cards. Explain how you
+	// would subclass the data structures to implement blackjack.
+	
+	public enum Suit{
+		Club (0), Diamond (1), Heart (2), Spade (3);
+		private int value;
+		private Suit(int v) { value = v; }
+		public int getValue() { return value;}
+		public static Suit getSuitFromValue(int value) { return null;  }
+	}
+	
+	public class Deck <T extends Card>{
+		private ArrayList<T> cards; // all cards
+		private int dealtIndex = 0; // marks first undealt card
+		
+		public void setDeckOfCards(ArrayList<T> deckOfCards) { }
+		
+		public void shuffle() { }
+		public int remainingCards() {
+			return cards.size() - dealtIndex;
+		}	
+		public T[] dealHand(int number) { return null; }
+		public T dealCard() { return null; }
+	}
+	
+	public abstract class Card {
+		private boolean available = true;
+		
+		// number or face on card - number 2 through 10, then 11 to 14
+		protected int faceValue;
+		protected Suit suit;
+		
+		public Card(int c, Suit s) {
+			faceValue = c;
+			suit = s;
+		}
+		
+		public abstract int value();
+		public Suit suit() { return suit; }
+		
+		// Checks if card is available to be given out
+		public boolean isAvailable( ) { return available; }
+		public void markUnavailable() { available = false; }
+		public void markAvailable() { available = true; }		
+	}
+	
+	public class Hand <T extends Card> {
+		protected ArrayList<T> cards = new ArrayList<T>();
+		
+		public int score() {
+			int score = 0;
+			for (T card : cards) {
+				score += card.value();
+			}
+			return score;
+		}
+		public void addCard(T card) {
+			cards.add(card);
+		}
+	}	
 
 }
