778141bc43f46f0a2f49360a973226e3cc9f4f77
Ha Hwang <spyderthing@gmail.com>
2020-07-10 16:47:05 -0400

    Added BlackJack class

--- ObjectOrientedDesign/src/main/java/me/nonnon/DeckOfCards.java
+++ ObjectOrientedDesign/src/main/java/me/nonnon/DeckOfCards.java
@@ -69,6 +69,40 @@ public class DeckOfCards {
 		public void addCard(T card) {
 			cards.add(card);
 		}
-	}	
+	}
+	
+	public class BlackJackHand extends Hand<BlackJackCard> {
+		/* There are multiple possible scores for ablackjack hand, since aces have
+		 * multiple values. Return the highest possible score that's under 21, or the
+		 * lowest score that's over. */
+		
+	}
+	
+	public class BlackJackCard extends Card {
+		public BlackJackCard(int c, Suit s) { super(c, s); }
+		public int value() {
+			if (isAce()) return 1;
+			else if (faceValue >= 11 && faceValue <= 13) return 10;
+			else return faceValue;
+		}
+		
+		public int minValue() {
+			if (isAce()) return 1;
+			else return value();
+		}
+		
+		public int maxValue() {
+			if (isAce()) return 11;
+			else return value();			
+		}
+		
+		public boolean isAce() {
+			return faceValue == 1;
+		}
+		
+		public boolean isFaceCard() {
+			return faceValue >= 11 && faceValue <= 13;
+		}
+	}		
 
 }
